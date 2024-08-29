c80a532750def9e3eba3546ccb84a9d0fcce2eba
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2024-08-29 06:30:46 +0200

    Remove LocationTable

--- Fuyu.Platform.Server/Databases/EftDatabase.cs
+++ Fuyu.Platform.Server/Databases/EftDatabase.cs
@@ -1,24 +1,13 @@
 using Fuyu.Platform.Common.IO;
-using Fuyu.Platform.Server.Databases.Tables;
 
 namespace Fuyu.Platform.Server.Databases
 {
     public static class EftDatabase
     {
-        public static readonly LocationTable Location;
-
-        static EftDatabase()
-        {
-            Location = new LocationTable();
-        }
-
         public static void Load()
         {
             // set data source
             Resx.SetSource("fuyu", typeof(EftDatabase).Assembly);
-
-            // load tables
-            Location.Load();
         }
     }
 }
