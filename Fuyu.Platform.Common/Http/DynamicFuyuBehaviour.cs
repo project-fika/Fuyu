0eeed938199c386d35c9d041db3071b5b63a3618
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-08-29 21:46:04 -0700

    Initial implementation

---
+++ Fuyu.Platform.Common/Http/DynamicFuyuBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fuyu.Platform.Common.Http
+{
+    public abstract class DynamicFuyuBehaviour : FuyuBehaviour
+    {
+        // ["", "files", "trader", "avatar", "{traderId}"]
+        public string[] PathSegments { get; set; }
+
+        // [{"traderId", 4}]
+        public List<DynamicPathSegment> DynamicSegments { get; set; }
+
+        // Logic is ran here in order to process before passing off to user implementation
+        public override void Run(FuyuContext context)
+        {
+            var args = new Dictionary<string, string>();
+            var split = context.GetPath().Split('/');
+            foreach (var dynamicSegment in DynamicSegments)
+            {
+                args[dynamicSegment.Name] = split[dynamicSegment.Index];
+            }
+
+            RunDynamic(context, args);
+        }
+
+        // args where the key is the path's name and value is taken from query by index so
+        // /files/trader/avatar/test would result in a dictionary of {{"traderId", "test"}}
+        public abstract void RunDynamic(FuyuContext context, Dictionary<string, string> args);
+
+        // No real reason to mark virtual other than modularity, checks the query to see
+        // if this behaviour can handle it where dynamic segments are ignored and if the
+        // rest of the query matches then the method will return true
+        public virtual bool CanHandle(string path)
+        {
+            var inputSegments = path.Split('/');
+            if (inputSegments.Length != PathSegments.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < inputSegments.Length; i++)
+            {
+                if (!IsDynamicSegment(i) && inputSegments[i] != PathSegments[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsDynamicSegment(int index)
+        {
+            return DynamicSegments.Any(segment => segment.Index == index);
+        }
+
+        public struct DynamicPathSegment
+        {
+            public string Name { get; }
+            public int Index { get; }
+
+            public DynamicPathSegment(string name, int index)
+            {
+                Name = name;
+                Index = index;
+            }
+        }
+    }
+}
--- Fuyu.Platform.Common/Http/FuyuServer.cs
+++ Fuyu.Platform.Common/Http/FuyuServer.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using WebSocketSharp.Server;
 using Fuyu.Platform.Common.IO;
+using static Fuyu.Platform.Common.Http.DynamicFuyuBehaviour;
+using System.Linq;
 
 namespace Fuyu.Platform.Common.Http
 {
@@ -9,6 +11,7 @@ namespace Fuyu.Platform.Common.Http
     {
         private readonly HttpServer _httpv;
         public readonly Dictionary<string, FuyuBehaviour> Behaviours;
+        public readonly List<DynamicFuyuBehaviour> DynamicBehaviours;
         public readonly FuyuBehaviour OnError; 
         public readonly string Address;
         public readonly string Name;
@@ -21,6 +24,7 @@ namespace Fuyu.Platform.Common.Http
             Name = name;
 
             Behaviours = new Dictionary<string, FuyuBehaviour>();
+            DynamicBehaviours = new List<DynamicFuyuBehaviour>();
             OnError = new ErrorBehaviour();
 
             _httpv = new HttpServer(uri.Port);
@@ -36,14 +40,13 @@ namespace Fuyu.Platform.Common.Http
 
             Terminal.WriteLine($"[{Name}] {path}");
 
-            if (Behaviours.ContainsKey(path))
-            {
-                Behaviours[path].Run(context);
-            }
-            else
-            {
-                OnError.Run(context);
-            }
+            FuyuBehaviour behaviour;
+            // If we can get the behaviour then return it otherwise check dynamic behaviours,
+            // if we still can't find any dynamic behaviour that can handle it then use OnError
+            // I do believe you will hate this code but I'm confident I won't touch it again
+            behaviour = Behaviours.TryGetValue(path, out behaviour) ? behaviour :
+                        DynamicBehaviours.FirstOrDefault(d => d.CanHandle(path)) ?? OnError;
+            behaviour.Run(context);
         }
 
         public void Start()
@@ -54,7 +57,37 @@ namespace Fuyu.Platform.Common.Http
 
         public void AddHttpService<T>(string path) where T : FuyuBehaviour, new()
         {
-            Behaviours.Add(path, new T());
+            var behaviour = new T();
+            if (behaviour is DynamicFuyuBehaviour dynamicBehaviour)
+            {
+                ProcessDynamicBehaviour(dynamicBehaviour, path);
+                DynamicBehaviours.Add(dynamicBehaviour);
+            }
+            else
+            {
+                Behaviours.Add(path, behaviour);
+            }
+        }
+
+        private void ProcessDynamicBehaviour(DynamicFuyuBehaviour dynamicBehaviour, string path)
+        {
+            var pathSegments = dynamicBehaviour.PathSegments = path.Split('/');
+            var dynamicSegments = dynamicBehaviour.DynamicSegments = new List<DynamicPathSegment>();
+
+            for (int i = 0; i < pathSegments.Length; i++)
+            {
+                var segment = pathSegments[i];
+                if (segment.StartsWith("{") && segment.EndsWith("}"))
+                {
+                    var pathName = segment.Trim('{', '}');
+                    dynamicSegments.Add(new DynamicPathSegment(pathName, i));
+                }
+            }
+
+            if (dynamicSegments.Count == 0)
+            {
+                throw new ArgumentException("Registered DynamicFuyuBehaviour but path is not dynamic", nameof(path));
+            }
         }
     }
 }
