2db63367cca03646b47f04cd65e440a7ff234232
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2024-09-01 16:10:53 +0200

    Add FuyuWsClient

---
+++ Fuyu.Platform.Common/Http/FuyuWsClient.cs
@@ -0,0 +1,80 @@
+using System;
+using WebSocketSharp;
+
+namespace Fuyu.Platform.Common.Http
+{
+    public class FuyuWsClient : IDisposable
+    {
+        private readonly string _url;
+        private WebSocket _ws;
+
+        public FuyuWsClient(string address, string path)
+        {
+            _url = address + path;
+        }
+
+        public void Start()
+        {
+            _ws = new WebSocket(_url)
+            {
+                EmitOnPing = true
+            };
+
+            _ws.OnMessage += OnMessage;
+            _ws.Connect();
+        }
+
+        public void Stop()
+        {
+            _ws.Close();
+        }
+
+        public void Send(string text)
+        {
+            _ws.Send(text);
+        }
+
+        public void Send(byte[] data)
+        {
+            _ws.Send(data);
+        }
+
+        private void OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.IsPing)
+            {
+                OnReceivePing();
+            }
+
+            if (e.IsText)
+            {
+                OnReceiveText(e.Data);
+            }
+
+            if (e.IsBinary)
+            {
+                OnReceiveBytes(e.RawData);
+            }
+        }
+
+        public virtual void OnReceivePing()
+        {
+            // intentionally empty
+        }
+
+        public virtual void OnReceiveText(string text)
+        {
+            // intentionally empty
+        }
+
+        public virtual void OnReceiveBytes(byte[] data)
+        {
+            // intentionally empty
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
