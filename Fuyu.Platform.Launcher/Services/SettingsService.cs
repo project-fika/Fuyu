92c8aaa0d74f28593deb09093d271023a03dd9d6
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2024-09-02 11:44:57 +0200

    Improve launcher page flow

--- Fuyu.Platform.Launcher/Services/SettingsService.cs
+++ Fuyu.Platform.Launcher/Services/SettingsService.cs
@@ -4,15 +4,25 @@ namespace Fuyu.Platform.Launcher.Services
 {
     public static class SettingsService
     {
-        public static string ClientDirectory;
+        // server settings
         public static string FuyuAddress;
         public static string EftAddress;
+        public static string ArenaAddress;
+
+        // client settings
+        public static string EftDirectory;
+        public static string ArenaDirectory;
 
         static SettingsService()
         {
-            ClientDirectory = Environment.CurrentDirectory;
+            // server settings
             FuyuAddress = "http://localhost:8000";
             EftAddress = "http://localhost:8001";
+            ArenaAddress = "http://localhost:8020";
+
+            // client settings
+            EftDirectory = Environment.CurrentDirectory;
+            ArenaDirectory = Environment.CurrentDirectory;
         }
     }
 }
