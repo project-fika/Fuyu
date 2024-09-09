c49ca449b6776ea741164baa21aa35d2e60c163c
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2024-09-09 13:21:29 +0200

    Remove redundant code

--- Fuyu.Common/Networking/WsController.cs
+++ Fuyu.Common/Networking/WsController.cs
@@ -1,7 +1,4 @@
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using Fuyu.Common.Compression;
 
 namespace Fuyu.Common.Networking
 {
@@ -12,35 +9,5 @@ namespace Fuyu.Common.Networking
         }
 
         public abstract Task RunAsync(WsContext context);
-
-        public static void Send(HttpContext context, byte[] data, string mime, bool zipped = true)
-        {
-            var response = context.Response;
-
-            // used for plaintext debugging
-            if (context.Request.Headers["fuyu-debug"] != null)
-            {
-                zipped = false;
-            }
-
-            if (zipped)
-            {
-                data = Zlib.Compress(data, ZlibCompression.Level9);
-            }
-
-            response.ContentType = mime;
-            response.ContentLength64 = data.Length;
-
-            using (var payload = response.OutputStream)
-            {
-                payload.Write(data, 0, data.Length);
-            }
-        }
-
-        public static void SendJson(HttpContext context, string text, bool zipped = true)
-        {
-            var encoded = Encoding.UTF8.GetBytes(text);
-            Send(context, encoded, "application/json; charset=utf-8", zipped);
-        }
     }
 }
