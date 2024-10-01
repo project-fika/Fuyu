d22fabf4f68cf88fdb8045688f2187df9292887f
TheSparta <8343238+TheSpartaPT@users.noreply.github.com>
2024-10-01 14:53:17 +0100

    Fixed bracket style to allman

--- Fuyu.Launcher/Store/GetGamesResultAction.cs
+++ Fuyu.Launcher/Store/GetGamesResultAction.cs
@@ -6,7 +6,8 @@ namespace Fuyu.Launcher.Store
     {
         public Dictionary<string, int?> Games { get; }
 
-        public GetGamesResultAction(Dictionary<string, int?> games) {
+        public GetGamesResultAction(Dictionary<string, int?> games)
+        {
             Games = games;
         }
     }
