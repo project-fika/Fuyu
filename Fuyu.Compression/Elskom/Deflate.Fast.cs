168ae3b22f338ac0e76bf0d86d63c56b69949907
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2024-10-02 13:08:50 +0200

    Move Elskom license details to LICENSE

--- Fuyu.Compression/Elskom/Deflate.Fast.cs
+++ Fuyu.Compression/Elskom/Deflate.Fast.cs
@@ -1,8 +1,3 @@
-// Copyright (c) 2018-2020, Els_kom org.
-// https://github.com/Elskom/
-// All rights reserved.
-// license: see LICENSE for more details.
-
 namespace Elskom.Generic.Libs
 {
     /// <summary>
