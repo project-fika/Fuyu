a45400bad87b497f6ca15a2feab319d3c57e5069
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-10-06 16:22:30 -0700

    Switched is to ==

--- Fuyu.Backend.EFT/ItemEvents/Controllers/EatItemEventController.cs
+++ Fuyu.Backend.EFT/ItemEvents/Controllers/EatItemEventController.cs
@@ -1,44 +1,42 @@
-﻿using Fuyu.Backend.BSG.ItemEvents;
-using Fuyu.Backend.EFT.DTO.Items;
-using Fuyu.Backend.EFT.DTO.Responses;
+﻿using System.Threading.Tasks;
+using Fuyu.Backend.BSG.ItemEvents;
 using Fuyu.Backend.BSG.ItemEvents.Controllers;
+using Fuyu.Backend.EFT.DTO.Items;
 using Fuyu.Backend.EFT.ItemEvents.Models;
 using Fuyu.Common.IO;
-using Fuyu.Common.Networking;
-using System.Threading.Tasks;
 
 namespace Fuyu.Backend.EFT.ItemEvents.Controllers
 {
-    public class EatItemEventController : ItemEventController<EatItemEvent>
-    {
-        public EatItemEventController() : base("Eat")
-        {
-        }
+	public class EatItemEventController : ItemEventController<EatItemEvent>
+	{
+		public EatItemEventController() : base("Eat")
+		{
+		}
 
-        public override Task RunAsync(ItemEventContext context, EatItemEvent request)
-        {
-            var account = EftOrm.GetAccount(context.SessionId);
-            var profile = EftOrm.GetProfile(account.PveId);
-            var index = 0;
-            ItemInstance item = null;
-            foreach (var _item in profile.Pmc.Inventory.items)
-            {
-                if (_item._id == request.Item)
-                {
-                    item = _item;
-                    break;
-                }
-             
-                index++;
-            }
+		public override Task RunAsync(ItemEventContext context, EatItemEvent request)
+		{
+			var account = EftOrm.GetAccount(context.SessionId);
+			var profile = EftOrm.GetProfile(account.PveId);
+			var index = 0;
+			ItemInstance item = null;
+			foreach (var _item in profile.Pmc.Inventory.items)
+			{
+				if (_item._id == request.Item)
+				{
+					item = _item;
+					break;
+				}
 
-            if (item is null)
-            {
-                Terminal.WriteLine($"Failed to find item {request.Item}");
-                return Task.CompletedTask;
-            }
-            
-            return Task.CompletedTask;
-        }
-    }
+				index++;
+			}
+
+			if (item == null)
+			{
+				Terminal.WriteLine($"Failed to find item {request.Item}");
+				return Task.CompletedTask;
+			}
+
+			return Task.CompletedTask;
+		}
+	}
 }
