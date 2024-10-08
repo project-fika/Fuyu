6754779933f8a0bd98202854087176fa37d034ee
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-10-07 18:13:57 -0700

    Added MongoId converter

---
+++ Fuyu.Common/Serialization/MongoIdConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Fuyu.Common.Hashing;
+using Newtonsoft.Json;
+
+namespace Fuyu.Common.Serialization
+{
+	public class MongoIdConverter : JsonConverter<MongoId>
+	{
+		public override MongoId ReadJson(JsonReader reader, Type objectType, MongoId existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			return new MongoId((string)reader.Value);
+		}
+
+		public override void WriteJson(JsonWriter writer, MongoId value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value.ToString());
+		}
+	}
+}
