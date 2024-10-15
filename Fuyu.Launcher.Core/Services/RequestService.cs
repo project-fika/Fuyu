3c764733aa4cf60931379c05431ce95272e2dbc9
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2024-10-15 02:31:00 +0200

    Change hardcoded wipe ID's

--- Fuyu.Launcher.Core/Services/RequestService.cs
+++ Fuyu.Launcher.Core/Services/RequestService.cs
@@ -1,15 +1,15 @@
-using System.Text;
+using System;
 using System.Collections.Generic;
-using Fuyu.Common.Collections;
-using Fuyu.Common.Hashing;
-using Fuyu.Common.Networking;
-using Fuyu.Common.Serialization;
+using System.Text;
 using Fuyu.Backend.Common.DTO.Requests;
 using Fuyu.Backend.Common.DTO.Responses;
 using Fuyu.Backend.Core.DTO.Accounts;
 using Fuyu.Backend.Core.DTO.Requests;
 using Fuyu.Backend.Core.DTO.Responses;
-using System;
+using Fuyu.Common.Collections;
+using Fuyu.Common.Hashing;
+using Fuyu.Common.Networking;
+using Fuyu.Common.Serialization;
 
 namespace Fuyu.Launcher.Core.Services
 {
@@ -26,20 +26,21 @@ namespace Fuyu.Launcher.Core.Services
             _httpClients.Set("arena", new EftHttpClient(SettingsService.ArenaAddress, string.Empty));
         }
 
-        private static void HttpPut<T1>(string id, string path, T1 request)
+        private static TResponse HttpGet<TResponse>(string id, string path)
 		{
 			if (!_httpClients.TryGet(id, out var httpc))
 			{
 				throw new Exception($"Id '{id}' not found");
 			}
 
-			var requestJson = Json.Stringify(request);
-            var requestBytes = Encoding.UTF8.GetBytes(requestJson);
+            var response = httpc.Get(path);
+            var responseJson = Encoding.UTF8.GetString(response.Body);
+            var responseValue = Json.Parse<TResponse>(responseJson);
 
-            httpc.Put(path, requestBytes);
+            return responseValue;
         }
 
-        private static T2 HttpPost<T1, T2>(string id, string path, T1 request)
+        private static TResponse HttpPost<TRequest, TResponse>(string id, string path, TRequest request)
 		{
 			if (!_httpClients.TryGet(id, out var httpc))
 			{
@@ -51,9 +52,22 @@ namespace Fuyu.Launcher.Core.Services
 
             var response = httpc.Post(path, requestBytes);
             var responseJson = Encoding.UTF8.GetString(response.Body);
-            var responseValue = Json.Parse<T2>(responseJson);
+            var responseValue = Json.Parse<TResponse>(responseJson);
 
             return responseValue;
+        }
+
+        private static void HttpPut<TRequest>(string id, string path, TRequest request)
+		{
+			if (!_httpClients.TryGet(id, out var httpc))
+			{
+				throw new Exception($"Id '{id}' not found");
+			}
+
+			var requestJson = Json.Stringify(request);
+            var requestBytes = Encoding.UTF8.GetBytes(requestJson);
+
+            httpc.Put(path, requestBytes);
         }
 
         public static void ResetSessions()
@@ -117,6 +131,15 @@ namespace Fuyu.Launcher.Core.Services
                 null);
 
             return response.Games;
+        }
+
+        public static string[] GetWipeProfiles()
+        {
+            var response = HttpGet<string[]>(
+                "eft",
+                "/fuyu/game/wipe/profiles");
+            
+            return response;
         }
 
         public static AccountRegisterGameResponse RegisterGame(string game, string edition)
